5c0389f8a6cde081927d865784d3517aa73f2bf3
Dineshkumar6301 <dk967427@gmail.com>
2026-05-30 12:03:16 +0530

    update

--- doctors/settings.py
+++ doctors/settings.py
@@ -15,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==============================
 
 SECRET_KEY = config("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    "hammerhead-app-jjkcf.ondigitalocean.app"
+    "hammerhead-app-jjkcf.ondigitalocean.app", "127.0.0.1"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -104,10 +104,27 @@ TEMPLATES = [
 ]
 
 # ==============================
+import dj_database_url
+import os
 
-DATABASES = {
-    "default": dj_database_url.parse(config("DATABASE_URL"), conn_max_age=600)
-}
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 # ==============================
 # PASSWORD VALIDATION
